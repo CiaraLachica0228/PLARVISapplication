fdf03ec711d3b8f5f777d800f7680430e2eba7bb
CiaraLachica0228 <ciaralachica54@gmail.com>
2017-10-27 22:10:24 +0800

    Initial commit

---
+++ app/src/main/java/com/cmclachicaplarvis/plarvis/RegistrationActivity.java
@@ -0,0 +1,33 @@
+package com.cmclachicaplarvis.plarvis;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+
+    private Button btnLinkToLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        btnLinkToLogin = (Button) findViewById(R.id.btnLinkToLoginScreen);
+
+        // Link to Login Screen
+        btnLinkToLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),
+                        LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
